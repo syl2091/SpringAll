036b9df26a5c4c428919ae01dc78af1c69783f51
onthroad <164378335@qq.com>
2021-07-14 17:28:01 +0800

    CRUD

--- 03.Spring-Boot-MyBatis/src/main/java/com/springboot/mapper/StudentMapper.java
+++ 03.Spring-Boot-MyBatis/src/main/java/com/springboot/mapper/StudentMapper.java
@@ -17,7 +17,7 @@ public interface StudentMapper {
 	@Insert("insert into student(sno,sname,ssex) values(#{sno},#{name},#{sex})")
 	int add(Student student);
 	
-	@Update("update student set sname=#{name},ssex=#{sex} where sno=#{sno}")
+	@Update("update sudent set sname=#{name},ssex=#{sex} where sno=#{sno}")
     int update(Student student);
 	
 	@Delete("delete from student where sno=#{sno}")
