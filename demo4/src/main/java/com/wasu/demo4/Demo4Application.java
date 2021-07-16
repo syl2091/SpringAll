48dc3e338bed2d04c019c0528a58a061a5aab45c
onthroad <164378335@qq.com>
2021-07-16 11:14:41 +0800

    Mybatis Mapper CRUD 多数据源配置

---
+++ demo4/src/main/java/com/wasu/demo4/Demo4Application.java
@@ -0,0 +1,14 @@
+package com.wasu.demo4;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo4Application.class, args);
+    }
+
+}
