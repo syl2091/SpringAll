519a916ac4b687d93655eeda7d6b1f17d79fba5c
onthroad <164378335@qq.com>
2021-07-23 16:10:25 +0800

    Spring admin server

---
+++ demo16/server/src/main/java/com/wasu/server/ServerApplication.java
@@ -0,0 +1,16 @@
+package com.wasu.server;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAdminServer
+public class ServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
+
+}
