fcb5bf77825f2e5158aaaabea31ae890d1cdb057
onthroad <164378335@qq.com>
2021-07-29 14:17:25 +0800

    eureka-server 注册中心

---
+++ demo21/eureka-server/src/test/java/com/wasu/demo21/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wasu.demo21;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
