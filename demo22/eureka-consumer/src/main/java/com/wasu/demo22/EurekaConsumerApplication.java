03ea01d8d91a0144eb3f22e006879d0974e0e6c5
onthroad <164378335@qq.com>
2021-08-02 17:10:45 +0800

    eureka-consumer 消费者

--- demo22/eureka-consumer/src/main/java/com/wasu/demo22/EurekaConsumerApplication.java
+++ demo22/eureka-consumer/src/main/java/com/wasu/demo22/EurekaConsumerApplication.java
@@ -2,12 +2,22 @@ package com.wasu.demo22;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class EurekaConsumerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EurekaConsumerApplication.class, args);
     }
 
+    @Bean
+    @LoadBalanced
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
