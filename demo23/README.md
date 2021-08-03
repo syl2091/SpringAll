f2b63f1824b6c68a2ed18ffcb86e6f57bf713277
onthroad <164378335@qq.com>
2021-08-03 15:12:04 +0800

    Hystrix Dashboard仪表盘

---
+++ demo23/README.md
@@ -0,0 +1,9 @@
+# 工程简介 
+## Hystrix Dashboard仪表盘
+启动 demo22 eureka-server eureka-client eureka-consumer
+
+eureka-consumer 暴露 hystrix.stream
+监控 http://localhost:9000/actuator/hystrix.stream
+
+# 延伸阅读
+
---
+++ demo23/src/test/java/com/wasu/demo23/HystrixDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wasu.demo23;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HystrixDashboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
