9b84ede6e004a560feaef9f8af4e2361f8ab8767
onthroad <164378335@qq.com>
2021-08-10 17:18:43 +0800

    config配置中心

---
+++ demo29/config-server/src/test/java/com/wasu/demo29/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wasu.demo29;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
