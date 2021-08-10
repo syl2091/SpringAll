b0fe710bb95e8ed2eb1d2548227d5ff367ee5b9d
onthroad <164378335@qq.com>
2021-08-10 09:47:37 +0800

    Zuul api网关

---
+++ demo28/feign-consumer/README.md
@@ -0,0 +1,7 @@
+# 工程简介
+Feign Consumer  
+Feign集成了Ribbon与Hystrix
+
+
+# 延伸阅读
+
---
+++ demo28/feign-consumer/src/main/java/com/wasu/demo25/service/UserService.java
@@ -0,0 +1,31 @@
+package com.wasu.demo25.service;
+
+import com.wasu.demo25.entity.User;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * @ClassName:UserService
+ * @Description: value指定服务提供者  fallback指定降级类
+ * @Author: Syl
+ * @Date: 2021/8/3 16:23
+ */
+@FeignClient(value = "Server-Provider", fallback = UserServiceFallback.class)
+public interface UserService {
+    @GetMapping("user/{id}")
+    public User get(@PathVariable("id") Long id);
+
+    @GetMapping("user")
+    public List<User> get();
+
+    @PostMapping("user")
+    public void add(@RequestBody User user);
+
+    @PutMapping("user")
+    public void update(@RequestBody User user);
+
+    @DeleteMapping("user/{id}")
+    public void delete(@PathVariable("id") Long id);
+}
