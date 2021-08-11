9c58e938de1bc07c7fee5cecb656de023f4c0c52
onthroad <164378335@qq.com>
2021-08-11 14:40:54 +0800

    Config配置中心 bus消息总线

--- demo29/README.md
+++ demo29/README.md
@@ -2,3 +2,7 @@
 
 # bus消息总线
 Spring Cloud Bus支持RabbitMQ和Kafka等消息中间件，这里只介绍配合RabbitMQ的使用方式。RabbitMQ的安装与启动可以参考https://mrbird.cc/Spring-Cloud-Hystrix-Dashboard.html中的介绍。
+
+# webhooks
+配合WebHooks可以实现自动配置刷新，在配置得到修改的时候它可以代替我们向Config-Server发送刷新请求。以码云为例，添加一个WebHooks：
+
--- demo29/config-client/src/main/java/com/wasu/demo29/controller/TestController.java
+++ demo29/config-client/src/main/java/com/wasu/demo29/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.wasu.demo29.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @Date: 2021/8/10 10:18
  */
 @RestController
+@RefreshScope
 public class TestController {
     @Value("${message}")
     private String message;
