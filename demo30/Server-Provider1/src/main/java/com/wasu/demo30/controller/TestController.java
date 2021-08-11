586b682eb01308f8b9071e247b7c634473934bdc
onthroad <164378335@qq.com>
2021-08-11 15:43:57 +0800

    sleuth zipkin

---
+++ demo30/Server-Provider1/src/main/java/com/wasu/demo30/controller/TestController.java
@@ -0,0 +1,30 @@
+package com.wasu.demo30.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+
+/**
+ * @ClassName:TestController
+ * @Description: TODO
+ * @Author: Syl
+ * @Date: 2021/8/11 14:55
+ */
+@RestController
+public class TestController {
+
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @GetMapping("/hello")
+    public String hello(){
+        logger.info("调用server1的hello接口");
+        return restTemplate.getForEntity("http://server-provider2/hello",String.class).getBody();
+    }
+}
---
+++ demo30/Server-Provider2/src/test/java/com/wasu/demo30/SleuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wasu.demo30;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SleuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
