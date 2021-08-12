05f424d765d50d4d9bb1d300ece3dc87d3ee3b7b
onthroad <164378335@qq.com>
2021-08-12 11:10:31 +0800

    dubbo zookeeper 远程调用

--- demo31/dubbo-boot/server-consumer/src/main/java/com/wasu/controller/HelloController.java
+++ demo31/dubbo-boot/server-consumer/src/main/java/com/wasu/controller/HelloController.java
@@ -2,6 +2,7 @@ package com.wasu.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.wasu.api.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,11 +16,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+
     @Reference
     private HelloService helloService;
 
     @GetMapping("/hello/{message}")
     public String hello(@PathVariable String message) {
-        return helloService.hello(message);
+        return this.helloService.hello(message);
     }
 }
